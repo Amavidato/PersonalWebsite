2f5dc20b362565a8cb991c9545a9f7fcc5c512b2
amato_davide <amatodavide1@gmail.com>
2022-03-09 21:01:23 +0100

    built scheleton schema

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import './App.css';
+import Header from './components/header/Header';
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
 
 function App() {
-  return <h1>...Currently Under Development...</h1>;
+    return (
+        <div>
+            <Header />
+            <About />
+            <Contact />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
